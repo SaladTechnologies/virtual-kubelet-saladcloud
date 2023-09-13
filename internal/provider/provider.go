8092e6b27eda565015ecdc1aeeeb07e969d0121e
priyansh <lucklypriyansh@gmail.com>
2023-09-13 16:38:07 +0200

    Provider changes working

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -16,7 +16,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"net/http"
 	"os"
 	"strings"
 	"time"
@@ -49,9 +48,9 @@ func NewSaladCloudProvider(ctx context.Context, inputVars models.InputVars) (*Sa
 	}
 	cloudProvider.setNodeCapacity()
 	cloudProvider.setCountryCodes([]string{"US"})
+
 	return cloudProvider, nil
 }
-
 func (p *SaladCloudProvider) setCountryCodes(countries []string) {
 	for _, countryCode := range countries {
 		p.countryCodes = append(p.countryCodes, saladclient.CountryCode(countryCode))
@@ -86,62 +85,125 @@ func (p *SaladCloudProvider) getNodeCapacity() corev1.ResourceList {
 func (p *SaladCloudProvider) CreatePod(ctx context.Context, pod *corev1.Pod) error {
 	ctx, span := trace.StartSpan(ctx, "CreatePod")
 	defer span.End()
-	log.G(ctx).Infof("Starting the process to create Pod: %s", pod.ObjectMeta.GetName())
+	log.G(ctx).Debug("creating a CreatePod")
 
-	if err := p.createAndExecuteContainerGroup(ctx, pod); err != nil {
+	createContainerObject := p.createContainersObject(pod)
+	createContainerGroup := p.createContainerGroup(createContainerObject, pod)
+
+	_, r, err := p.apiClient.
+		ContainerGroupsAPI.CreateContainerGroup(
+		p.contextWithAuth(),
+		p.inputVars.OrganizationName,
+		p.inputVars.ProjectName).CreateContainerGroup(
+		createContainerGroup[0],
+	).Execute()
+	if err != nil {
+		log.G(ctx).Errorf("Error when calling `ContainerGroupsAPI.CreateContainerGroupModel`", r)
 		return err
 	}
-	// wait for 3 seconds to start the container
+
+	// wait for 3 second
 	time.Sleep(3 * time.Second)
 
-	if err := p.startContainerGroup(ctx, pod); err != nil {
-		log.G(ctx).Errorf("Error starting the container: %s", pod.ObjectMeta.GetName(), err.Error())
+	startHttpResponse, err := p.apiClient.ContainerGroupsAPI.StartContainerGroup(p.contextWithAuth(), p.inputVars.OrganizationName, p.inputVars.ProjectName, utils.GetPodName(pod.Namespace, pod.Name, nil)).Execute()
+	if err != nil {
+		log.G(ctx).Errorf("Error when Starting the container ", startHttpResponse)
 		return err
 	}
 
-	p.updatePodStatus(pod)
+	now := metav1.NewTime(time.Now())
+	pod.ObjectMeta.CreationTimestamp = now
+	pod.Status = corev1.PodStatus{
+		Phase:     corev1.PodRunning,
+		StartTime: &now,
+		Conditions: []corev1.PodCondition{
+			{
+				Type:   corev1.PodInitialized,
+				Status: corev1.ConditionTrue,
+			},
+			{
+				Type:   corev1.PodReady,
+				Status: corev1.ConditionTrue,
+			},
+			{
+				Type:   corev1.PodScheduled,
+				Status: corev1.ConditionTrue,
+			},
+		},
+	}
+	for _, container := range pod.Spec.Containers {
+		pod.Status.ContainerStatuses = append(pod.Status.ContainerStatuses, corev1.ContainerStatus{
+			Name:         container.Name,
+			Image:        container.Image,
+			Ready:        true,
+			RestartCount: 0,
+		})
+	}
 
-	log.G(ctx).Infof("Successfully created and started Pod: %s", pod.Name)
-	//p.notifier(pod)
+	log.G(ctx).Infof("Done creating the container and initiating  the startup ", pod)
 	return nil
 }
 
 func (p *SaladCloudProvider) UpdatePod(ctx context.Context, pod *corev1.Pod) error {
-	log.G(ctx).Infof("Called the Update Pod : %s", pod.ObjectMeta.GetName())
-	//p.notifier(pod)
 	return nil
 }
 
 func (p *SaladCloudProvider) DeletePod(ctx context.Context, pod *corev1.Pod) error {
 	ctx, span := trace.StartSpan(ctx, "DeletePod")
 	defer span.End()
-
-	log.G(ctx).Infof("Starting the deletion process for Pod: %s", pod.ObjectMeta.GetName())
-
-	if err := p.executeContainerGroupDeletion(ctx, pod); err != nil {
-		log.G(ctx).Infof("Failed to delete the container : %s", pod.ObjectMeta.GetName(), err.Error())
+	log.G(ctx).Debug("deleting a pod")
+	response, err := p.apiClient.ContainerGroupsAPI.DeleteContainerGroup(p.contextWithAuth(), p.inputVars.OrganizationName, p.inputVars.ProjectName, utils.GetPodName(pod.Namespace, pod.Name, pod)).Execute()
+	pod.Status.Phase = corev1.PodSucceeded
+	pod.Status.Reason = "Pod Deleted"
+	if err != nil {
+		log.G(ctx).Errorf("Error when deleting the container ", response)
 		return err
 	}
-
-	p.updatePodStatusAfterDeletion(ctx, pod)
-	//p.notifier(pod)
-	log.G(ctx).Infof("Successfully deleted the container for Pod: %s", pod.Name)
+	now := metav1.Now()
+	for idx := range pod.Status.ContainerStatuses {
+		pod.Status.ContainerStatuses[idx].Ready = false
+		pod.Status.ContainerStatuses[idx].State = corev1.ContainerState{
+			Terminated: &corev1.ContainerStateTerminated{
+				Message:    "Salad Provider Pod Deleted",
+				FinishedAt: now,
+				Reason:     "Salad Provider Pod Deleted",
+			},
+		}
+	}
+	log.G(ctx).Infof("Done deleting the container ", pod)
 	return nil
 }
 
 func (p *SaladCloudProvider) GetPod(ctx context.Context, namespace string, name string) (*corev1.Pod, error) {
-	log.G(ctx).Infof("Fetching details for Pod: %s in Namespace: %s", name, namespace)
 
-	apiClient := saladclient.NewAPIClient(saladclient.NewConfiguration())
-	resp, r, err := apiClient.ContainerGroupsAPI.GetContainerGroup(p.contextWithAuth(), p.inputVars.OrganizationName, p.inputVars.ProjectName, utils.GetPodName(namespace, name, nil)).Execute()
+	resp, r, err := saladclient.NewAPIClient(saladclient.NewConfiguration()).ContainerGroupsAPI.GetContainerGroup(p.contextWithAuth(), p.inputVars.OrganizationName, p.inputVars.ProjectName, utils.GetPodName(namespace, name, nil)).Execute()
 	if err != nil {
-		log.G(ctx).Errorf("Error when calling `ContainerGroupsAPI.GetPod`: %v", r)
-		return nil, p.handleStatusCodeError(ctx, r, err) // Assuming you have handleStatusCodeError function from previous refactoring
+		log.G(ctx).Errorf("Error when calling `ContainerGroupsAPI.GetPod`", r)
+		return nil, err
+	}
+	startTime := metav1.NewTime(resp.CreateTime)
+	pod := &corev1.Pod{
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:  resp.Name,
+					Image: resp.Container.Image,
+				},
+			},
+		},
+		Status: corev1.PodStatus{
+			Phase:     utils.GetPodPhaseFromContainerGroupState(resp.CurrentState),
+			StartTime: &startTime,
+			ContainerStatuses: []corev1.ContainerStatus{
+				{
+					Name:  resp.Name,
+					Image: resp.Container.Image,
+					Ready: utils.GetPodPhaseFromContainerGroupState(resp.CurrentState) == corev1.PodRunning,
+				},
+			},
+		},
 	}
 
-	pod := p.convertResponseToPod(resp)
-
-	log.G(ctx).Infof("Successfully fetched details for Pod: %s", name)
 	return pod, nil
 }
 
@@ -219,12 +281,6 @@ func (p *SaladCloudProvider) GetPods(ctx context.Context) ([]*corev1.Pod, error)
 
 	}
 	return pods, nil
-}
-
-// NotifyPods is called to set a pod notifier callback function. This should be called before any operations are done
-// within the provider.
-func (p *SaladCloudProvider) NotifyPods(ctx context.Context, notifier func(*corev1.Pod)) {
-	p.notifier = notifier
 }
 
 func (p *SaladCloudProvider) GetContainerLogs(ctx context.Context, namespace, podName, containerName string, opts nodeapi.ContainerLogOpts) (io.ReadCloser, error) {
@@ -305,154 +361,4 @@ func (p *SaladCloudProvider) createContainerGroup(createContainerList []saladcli
 
 	return createContainerGroups
 
-}
-
-func (p *SaladCloudProvider) createAndExecuteContainerGroup(ctx context.Context, pod *corev1.Pod) error {
-	log.G(ctx).Debug("Creating container objects...")
-	createContainerObject := p.createContainersObject(pod)
-
-	log.G(ctx).Debug("Creating container group...")
-	createContainerGroup := p.createContainerGroup(createContainerObject, pod)
-
-	_, r, err := p.apiClient.ContainerGroupsAPI.CreateContainerGroup(
-		p.contextWithAuth(),
-		p.inputVars.OrganizationName,
-		p.inputVars.ProjectName).CreateContainerGroup(
-		createContainerGroup[0],
-	).Execute()
-
-	if err != nil {
-		log.G(ctx).Errorf("Failed to create ContainerGroup for Pod: %s. Error: %v", pod.Name, r)
-		return p.handleStatusCodeError(ctx, r, err)
-	}
-
-	log.G(ctx).Infof("Successfully created ContainerGroup for Pod: %s", pod.Name)
-	return nil
-}
-
-func (p *SaladCloudProvider) startContainerGroup(ctx context.Context, pod *corev1.Pod) error {
-	log.G(ctx).Debugf("Starting container for Pod: %s...", pod.ObjectMeta.GetName())
-	resp, err := p.apiClient.ContainerGroupsAPI.StartContainerGroup(p.contextWithAuth(), p.inputVars.OrganizationName, p.inputVars.ProjectName, utils.GetPodName(pod.Namespace, pod.Name, nil)).Execute()
-	if err != nil {
-		log.G(ctx).Errorf("Failed to start container for Pod: %s. Response: %v", pod.ObjectMeta.GetName(), resp)
-		return p.handleStatusCodeError(ctx, resp, err)
-	}
-
-	log.G(ctx).Infof("Successfully started container for Pod: %s", pod.Name)
-	return nil
-}
-
-func (p *SaladCloudProvider) handleStatusCodeError(ctx context.Context, resp *http.Response, err error) error {
-	errorMsg := "Unknown error occurred"
-
-	if resp != nil {
-		switch resp.StatusCode {
-		case http.StatusNotFound:
-			errorMsg = "Resource not found"
-		case http.StatusForbidden:
-			errorMsg = "Permission denied"
-		case http.StatusInternalServerError:
-			errorMsg = "Internal server error"
-		case http.StatusConflict:
-			errorMsg = "Resource conflict"
-		default:
-			errorMsg = fmt.Sprintf("Unexpected status code %d", resp.StatusCode)
-		}
-	}
-
-	log.G(ctx).Errorf("%s: %v", errorMsg, err)
-	return fmt.Errorf("%s: %w", errorMsg, err)
-}
-func (p *SaladCloudProvider) updatePodStatus(pod *corev1.Pod) {
-	now := metav1.NewTime(time.Now())
-	pod.ObjectMeta.CreationTimestamp = now
-	pod.Status = corev1.PodStatus{
-		Phase:     corev1.PodPending,
-		StartTime: &now,
-		Conditions: []corev1.PodCondition{
-			{
-				Type:   corev1.PodInitialized,
-				Status: corev1.ConditionTrue,
-			},
-			{
-				Type:   corev1.PodReady,
-				Status: corev1.ConditionTrue,
-			},
-			{
-				Type:   corev1.PodScheduled,
-				Status: corev1.ConditionTrue,
-			},
-		},
-	}
-	for _, container := range pod.Spec.Containers {
-		pod.Status.ContainerStatuses = append(pod.Status.ContainerStatuses, corev1.ContainerStatus{
-			Name:         container.Name,
-			Image:        container.Image,
-			Ready:        true,
-			RestartCount: 0,
-			State: corev1.ContainerState{
-				Running: &corev1.ContainerStateRunning{
-					StartedAt: now,
-				},
-			},
-		})
-	}
-}
-func (p *SaladCloudProvider) executeContainerGroupDeletion(ctx context.Context, pod *corev1.Pod) error {
-	log.G(ctx).Debug("Executing container group deletion...")
-
-	response, err := p.apiClient.ContainerGroupsAPI.DeleteContainerGroup(p.contextWithAuth(), p.inputVars.OrganizationName, p.inputVars.ProjectName, utils.GetPodName(pod.Namespace, pod.Name, pod)).Execute()
-	if err != nil {
-		log.G(ctx).Errorf("Failed to delete the container for Pod: %s. Response: %v", pod.Name, response)
-		return fmt.Errorf("container deletion error: %w", err)
-	}
-
-	return nil
-}
-
-func (p *SaladCloudProvider) updatePodStatusAfterDeletion(ctx context.Context, pod *corev1.Pod) {
-	log.G(ctx).Debugf("Updating Pod status post deletion for Pod: %s", pod.Name)
-
-	pod.Status.Phase = corev1.PodSucceeded
-	pod.Status.Reason = "Pod Deleted"
-
-	now := metav1.Now()
-	for idx := range pod.Status.ContainerStatuses {
-		pod.Status.ContainerStatuses[idx].Ready = false
-		pod.Status.ContainerStatuses[idx].State = corev1.ContainerState{
-			Terminated: &corev1.ContainerStateTerminated{
-				Message:    "Salad Provider Pod Deleted",
-				FinishedAt: now,
-				Reason:     "Salad Provider Pod Deleted",
-			},
-		}
-	}
-}
-
-func (p *SaladCloudProvider) convertResponseToPod(resp *saladclient.ContainerGroup) *corev1.Pod {
-	startTime := metav1.NewTime(resp.CreateTime)
-
-	pod := &corev1.Pod{
-		Spec: corev1.PodSpec{
-			Containers: make([]corev1.Container, 1),
-		},
-		Status: corev1.PodStatus{
-			Phase:             utils.GetPodPhaseFromContainerGroupState(resp.CurrentState),
-			StartTime:         &startTime,
-			ContainerStatuses: make([]corev1.ContainerStatus, 1),
-		},
-	}
-	pod.Spec.Containers = make([]corev1.Container, 1)
-
-	pod.Spec.Containers[0] = corev1.Container{
-		Name:  resp.Name,
-		Image: resp.Container.Image,
-	}
-	pod.Status.ContainerStatuses[0] = corev1.ContainerStatus{
-		Name:  resp.Name,
-		Image: resp.Container.Image,
-		Ready: utils.GetPodPhaseFromContainerGroupState(resp.CurrentState) == corev1.PodRunning,
-	}
-
-	return pod
 }
